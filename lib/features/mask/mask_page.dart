c654d4ffc85a5726f6cdbf1aee7b677a43009b0f
LinXunFeng <linxunfeng@yeah.net>
2022-04-10 18:58:10 +0800

    <feat> (遮罩): 滚动到指定模块后展示遮罩

--- lib/features/mask/mask_page.dart
+++ lib/features/mask/mask_page.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
-class MaskPage extends StatelessWidget {
+class MaskPage extends StatefulWidget {
+  const MaskPage({Key? key}) : super(key: key);
+
+  @override
+  State<MaskPage> createState() => _MaskPageState();
+}
+
+class _MaskPageState extends State<MaskPage> {
   OverlayEntry? overlayEntry;
-  final globalKey = GlobalKey();
+
+  final observeScrollGlobalKey = GlobalKey();
+
+  final maskTargetGlobalKey = GlobalKey();
+
+  final listViewController = ItemScrollController();
+  final listItemPositionsListener = ItemPositionsListener.create();
 
   ScrollMetrics? listViewMetrics;
-  double appBarHeight = AppBar().preferredSize.height;
 
-  MaskPage({Key? key}) : super(key: key);
+  double appBarHeight = AppBar().preferredSize.height;
+  double safeAreaTopHeight = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    listItemPositionsListener.itemPositions.addListener(() {
+      _observeScroll();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    listItemPositionsListener.itemPositions.removeListener(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    safeAreaTopHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       appBar: AppBar(title: const Text("遮罩")),
-      body: _buildListView(),
+      body: _buildListView(context),
       floatingActionButton: _buildFloatingActionButton(context),
     );
   }
@@ -22,18 +52,27 @@ class MaskPage extends StatelessWidget {
     return FloatingActionButton(
       child: const Icon(Icons.add),
       onPressed: () {
-        _showMask(context);
+        debugPrint(
+            "itemPositions -- ${listItemPositionsListener.itemPositions.value}");
       },
     );
   }
 
-  Widget _buildListView() {
-    Widget resultWidget = ListView.separated(
+  Widget _buildListView(BuildContext context) {
+    Widget resultWidget = ScrollablePositionedList.separated(
+      itemScrollController: listViewController,
+      itemPositionsListener: listItemPositionsListener,
       itemCount: 100,
       itemBuilder: (context, index) {
+        Key? key;
+        if (index == 16) {
+          key = observeScrollGlobalKey;
+        } else if (index == 20) {
+          key = maskTargetGlobalKey;
+        }
         return ListTile(
           title: Text("$index"),
-          key: index == 20 ? globalKey : null,
+          key: key,
         );
       },
       separatorBuilder: (context, index) {
@@ -54,10 +93,40 @@ class MaskPage extends StatelessWidget {
     return resultWidget;
   }
 
-  void _showMask(BuildContext context) {
+  void _observeScroll() {
+    var renderBox = observeScrollGlobalKey.currentContext?.findRenderObject();
+    if (renderBox is RenderBox?) {
+      var globalOffsetY = appBarHeight + safeAreaTopHeight;
+      var offset = renderBox?.localToGlobal(Offset(0, -globalOffsetY));
+      if (offset == null) {
+        return;
+      }
+      var _listViewMetrics = listViewMetrics;
+      if (_listViewMetrics == null) {
+        return;
+      }
+      double cellOffsetY = offset.dy;
+      if (cellOffsetY >= 0) {
+        return;
+      }
+
+      debugPrint("cellOffsetY -- $cellOffsetY");
+      // 滚动指定下标模块(alignment可设置偏移系数，依据listview的高度，范围[-1.0, 1.0])
+      // listViewController.jumpTo(index: 16, alignment: 0.1);
+      listViewController.jumpTo(index: 16);
+      Future.delayed(const Duration(milliseconds: 100), () {
+        _showMask();
+      });
+    }
+  }
+
+  void _showMask() {
+    if (overlayEntry != null) {
+      return;
+    }
     Size size = Size.zero;
     Offset offset = Offset.zero;
-    var renderBox = globalKey.currentContext?.findRenderObject();
+    var renderBox = maskTargetGlobalKey.currentContext?.findRenderObject();
     if (renderBox is RenderBox?) {
       var _size = renderBox?.size;
       var _offset = renderBox?.localToGlobal(Offset.zero);
@@ -96,53 +165,73 @@ class MaskPage extends StatelessWidget {
     }
     overlayEntry = OverlayEntry(
       builder: (context) {
-        return Stack(
-          children: [
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                Colors.red.withOpacity(0.8),
-                BlendMode.srcOut,
-              ),
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      /// 任何颜色均可
-                      color: Colors.white,
-                      backgroundBlendMode: BlendMode.dstOut,
-                    ),
-                  ),
-                  Positioned(
-                    left: offset.dx,
-                    top: offset.dy,
-                    child: Container(
+        return Material(
+          color: Colors.transparent,
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  Colors.red.withOpacity(0.8),
+                  BlendMode.srcOut,
+                ),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Container(
                       decoration: const BoxDecoration(
+                        // 任何颜色均可
                         color: Colors.white,
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        backgroundBlendMode: BlendMode.dstOut,
                       ),
-                      width: size.width,
-                      height: size.height,
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              left: offset.dx,
-              top: offset.dy + 100,
-              child: GestureDetector(
-                onTap: () {
-                  overlayEntry?.remove();
-                },
-                child: Container(
-                  height: size.height,
-                  padding: const EdgeInsets.all(8),
-                  color: Colors.white,
-                  child: const Text("知道了", style: TextStyle(fontSize: 20, color: Colors.black),),
+                    Positioned(
+                      top: offset.dy - 80,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          /// 任何颜色均可
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                        ),
+                        width: 150,
+                        height: size.height,
+                      ),
+                    ),
+                    Positioned(
+                      left: offset.dx,
+                      top: offset.dy,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          /// 任何颜色均可
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        width: size.width,
+                        height: size.height,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Positioned(
+                top: offset.dy + 100,
+                child: GestureDetector(
+                  onTap: () {
+                    overlayEntry?.remove();
+                    overlayEntry = null;
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.white,
+                    child: const Text(
+                      "知道了",
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
